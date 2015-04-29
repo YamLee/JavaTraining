e1fdd4893cd27b394c199410fae3ed93c058fb17
Liyan <liyan@readingjoy.com>
2015-04-29 19:05:23 +0800

    commit some ide files

--- src/com/yamlee/thread/ThreadYield.java
+++ src/com/yamlee/thread/ThreadYield.java
@@ -5,9 +5,24 @@ package com.yamlee.thread;
  */
 public class ThreadYield {
     public static void main(String[] args){
-            for (int i=0;i<10;i++){
+            for (int i=0;i<3;i++){
                 Thread thread = new Thread(new YieldRunnable(i));
+                if(i==0) {
+                    thread.setPriority(Thread.MAX_PRIORITY);
+                }
+                if(i==1){
+                    thread.setPriority(Thread.NORM_PRIORITY);
+                }
+                if(i == 2){
+                    thread.setPriority(Thread.MIN_PRIORITY);
+                }
                 thread.start();
+                thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+                    @Override
+                    public void uncaughtException(Thread t, Throwable e) {
+
+                    }
+                });
             }
     }
 
@@ -22,7 +37,7 @@ public class ThreadYield {
         public void run() {
             System.out.println("-------run---------"+i);
             Thread.yield();
-            Thread.yield();
+//            Thread.yield();
 //            Thread.yield();
             System.out.println("-------end---------"+i);
         }
