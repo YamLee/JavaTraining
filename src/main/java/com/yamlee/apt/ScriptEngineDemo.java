b997daae9d19d1ce38d7664af6439101100ab49f
YamLee <liyaninhn@gmail.com>
2017-02-20 15:37:56 +0800

    Add apt demo

---
+++ src/main/java/com/yamlee/apt/ScriptEngineDemo.java
@@ -0,0 +1,24 @@
+package com.yamlee.apt;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import java.util.List;
+
+/**
+ * Created by yamlee on 9/30/16.
+ */
+public class ScriptEngineDemo {
+    public static void main(String[] args) {
+        ScriptEngineManager engineManager = new ScriptEngineManager();
+        List<ScriptEngineFactory> engineFactories = engineManager.getEngineFactories();
+        for (int i = 0; i < engineFactories.size(); i++) {
+            ScriptEngineFactory scriptEngineFactory = engineFactories.get(i);
+            System.out.println("engine name:" + scriptEngineFactory.getEngineName());
+            System.out.println("language name:" + scriptEngineFactory.getLanguageName());
+        }
+        ScriptEngine scriptEngine = engineManager.getEngineByName("JavaScript");
+        System.out.println("JavaScript engine is Null:" + scriptEngine == null);
+
+    }
+}
